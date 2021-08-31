73e16492816958a985a6c0906e68eed509444263
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2021-08-31 20:18:52 +0200

    Developed SplashScreen.dart and SecondSplashScreen.dart and LoginScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:spa_app/login_register_screens/LoginScreen.dart';
+import 'package:spa_app/login_register_screens/SecondSplashScreen.dart';
 import 'package:spa_app/login_register_screens/SplashScreen.dart';
 
 void main() {
   runApp(MyApplication());
 }
 
-
 class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {SplashScreen.ROUTE_NAME: (context) => SplashScreen()},
+      routes: {
+        SplashScreen.ROUTE_NAME: (context) => SplashScreen(),
+        SecondSplashScreen.ROUTE_NAME: (context) => SecondSplashScreen(),
+        LoginScreen.ROUTE_NAME: (context) => LoginScreen()
+      },
       initialRoute: SplashScreen.ROUTE_NAME,
     );
   }
